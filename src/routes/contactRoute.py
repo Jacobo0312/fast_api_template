345ffd0f754dd85171d9f66d0e1f18733cc5b236
Jacobo Garcia <72984810+Jacobo0312@users.noreply.github.com>
2023-06-14 22:47:16 -0500

    Add create and get all tasks

--- src/routes/contactRoute.py
+++ src/routes/contactRoute.py
@@ -5,6 +5,9 @@ from hubspot.crm.contacts.exceptions import ApiException
 from src.database.db import conn
 from src.models.contactModel import contacts
 from src.schemas.contactSchema import Contact
+from decouple import config
+import requests
+import json
 
 
 contactRouter = APIRouter()
@@ -36,6 +39,83 @@ async def get_contact(id:str):
         return contact_fetched.properties
     except ApiException as e:
         return ("Exception when requesting contact by id: %s\n" % e)
+    
+
+#Get all contacts
+@contactRouter.get("/contacts")
+async def get_all_contacts():
+    try:
+        all_contacts = hubspotClient.crm.contacts.get_all()
+        contact_list = []
+        for contact in all_contacts:
+            contact_list.append(contact.properties)
+        return contact_list   
+    except ApiException as e:
+        return ("Exception when requesting all contacts: %s\n" % e)
+
+
+import requests
+
+
+@contactRouter.get("/tasks")
+async def get_all_tasks():
+    list_id = config('CLICKUP_LIST_ID')
+    url = "https://api.clickup.com/api/v2/list/" + list_id + "/task"
+
+    headers = {"Authorization": config('CLICKUP_TOKEN')}
+
+    response = requests.get(url, headers=headers)
+
+    data = response.json()
+    return data
+
+@contactRouter.post("/tasks")
+async def create_task():
+    list_id = config('CLICKUP_LIST_ID')
+    url = "https://api.clickup.com/api/v2/list/" + list_id + "/task"
+
+    payload = {
+    "name": "New Task Name",
+    "description": "New Task Description",
+    "assignees": [
+        183
+    ],
+    "tags": [
+        "tag name 1"
+    ],
+    "status": "Open",
+    "priority": 3,
+    "due_date": 1508369194377,
+    "due_date_time": False,
+    "time_estimate": 8640000,
+    "start_date": 1567780450202,
+    "start_date_time": False,
+    "notify_all": True,
+    "parent": None,
+    "links_to": None,
+    "check_required_custom_fields": True,
+    "custom_fields": [
+        {
+        "id": "0a52c486-5f05-403b-b4fd-c512ff05131c",
+        "value": "This is a string of text added to a Custom Field."
+        }
+    ]
+    }
+
+    headers = {
+    "Content-Type": "application/json",
+    "Authorization": config('CLICKUP_TOKEN')
+    }
+
+    response = requests.post(url, json=payload, headers=headers, params=query)
+
+    data = response.json()
+    print(data)
+    return data
+    
+
+
+
 
 
     
